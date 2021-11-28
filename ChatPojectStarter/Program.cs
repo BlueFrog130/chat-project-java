b37d6a1d074c22da31a3ef47e8fc4d28b15b892d
Adam Grady <Adam.Grady@live.com>
2021-11-28 13:22:03 -0600

    Add project files.

---
+++ ChatPojectStarter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace ChatPojectClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using Process process = new Process();
+            process.StartInfo.WorkingDirectory = "Java";
+            process.StartInfo.FileName = "java";
+            process.StartInfo.Arguments = "--enable-preview --module-path lib --add-modules javafx.controls,javafx.fxml -jar ChatProjectClient.jar";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
---
+++ ChatProjectServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ChatProjectServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using Process process = new Process();
+            process.StartInfo.WorkingDirectory = "Java";
+            process.StartInfo.FileName = "java";
+            process.StartInfo.Arguments = "--enable-preview -jar ChatProject.jar";
+            process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+            process.ErrorDataReceived += (sender, e) => Console.WriteLine(e.Data);
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+        }
+    }
+}
